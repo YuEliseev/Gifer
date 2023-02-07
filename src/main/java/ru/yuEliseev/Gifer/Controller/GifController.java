7401c390ed8152d6e189a3039a37a218e87dedbf
Юрий <swordandshealdmaster@gmail.com>
2023-02-08 00:00:02 +0400

    add new method to GifClient - getGifBySearch new class Gif new views view list refactored GifService GifController need to refactor GifService

--- src/main/java/ru/yuEliseev/Gifer/Controller/GifController.java
+++ src/main/java/ru/yuEliseev/Gifer/Controller/GifController.java
@@ -3,22 +3,34 @@ package ru.yuEliseev.Gifer.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import ru.yuEliseev.Gifer.Service.GifService.GifService;
 
 @Controller
+@RequestMapping("/gif")
 public class GifController {
 
     @Autowired
     private GifService gifService;
 
-    @GetMapping("/{tag}")
-    public String home(@PathVariable("tag") String tag, Model model){
+    @GetMapping("/random/{tag}")
+    public String random(@PathVariable("tag") String tag, Model model){
+        model.addAttribute("gif", gifService.getRandom(tag));
 
-        model.addAttribute("preview", gifService.GetPreview(tag));
+        return "view";
+    }
+    @GetMapping("/search/{search}")
+    public String search(@PathVariable("search") String search,
+                         Model model){
+        model.addAttribute("gifList", gifService.getSearchingResults(search));
 
-        return "home";
+        return "list";
+    }
+    @GetMapping("/get/{id}")
+    public String getByID(@PathVariable("id") String id, Model model){
+        model.addAttribute("gif", gifService.getById(id));
+
+        return "view";
     }
 
 
--- src/main/java/ru/yuEliseev/Gifer/Service/GifService/GifService.java
+++ src/main/java/ru/yuEliseev/Gifer/Service/GifService/GifService.java
@@ -1,12 +1,16 @@
 package ru.yuEliseev.Gifer.Service.GifService;
 
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yuEliseev.Gifer.Client.GifClient;
 import ru.yuEliseev.Gifer.Config.GifClientConfig;
+import ru.yuEliseev.Gifer.Config.GifRequestConfig;
+import ru.yuEliseev.Gifer.Model.Gif;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class GifService {
@@ -14,19 +18,41 @@ public class GifService {
     @Autowired
     private GifClientConfig gifClientConfig;
     @Autowired
+    private GifRequestConfig gifRequestConfig;
+    @Autowired
     private GifClient gifClient;
 
-    public String GetPreview(String tag){
+    public Gif getRandom(String tag){
+        JSONObject jsonResponse = new JSONObject(gifClient.getRandomGif(gifClientConfig.getApiKey(), tag));
 
-        String jsonString = gifClient.GetGifJSON(gifClientConfig.getApiKey(), tag);
-
-        JSONObject jsonObject = new JSONObject(jsonString);
-
-        return jsonObject.getJSONObject("data")
-                .getJSONObject("images")
-                .getJSONObject("original")
-                .get("url")
-                .toString();
+        return new Gif(jsonResponse.getJSONObject("data"));
     }
 
+
+    public List<Gif> getSearchingResults(String search){
+        JSONObject jsonResponse = new JSONObject(gifClient.getGifBySearch(gifClientConfig.getApiKey(),
+                search,
+                gifRequestConfig.getRows()));
+
+        System.out.println(gifRequestConfig.getRows());
+
+        List<Gif> gifList = new ArrayList<>();
+
+        JSONArray jsonArray = jsonResponse.getJSONArray("data");
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            gifList.add(new Gif(jsonArray.getJSONObject(i)));
+        }
+
+        return gifList;
+
+    }
+
+    public Gif getById(String id){
+
+        JSONObject jsonResponse = new JSONObject(gifClient.getGifById(gifClientConfig.getApiKey(), id));
+        return new Gif((JSONObject) jsonResponse.getJSONArray("data").get(0));
+    }
+    //need to refactor this
 }
+
